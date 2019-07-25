b5d4e502b30fd8333639ab3da2fd410209fa38ad
rui li <rli925@usc.edu>
2019-07-25 10:50:54 -0700

    Updates

---
+++ precache-manifest.6d382e10e48fe0e7bf3caf6a3615bd5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443b66a502fc4ecd319638e39accc9a3",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "bc69b4663ba7e1b75358",
+    "url": "/Monster-Rolodex/static/css/main.7a219f91.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bc69b4663ba7e1b75358",
+    "url": "/Monster-Rolodex/static/js/main.29dd2611.chunk.js"
+  },
+  {
+    "revision": "1c53a097523ea440329e",
+    "url": "/Monster-Rolodex/static/js/runtime~main.5e73536d.js"
+  }
+]);
